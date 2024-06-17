dab162b249d77480a73cfee693a16d6ecee98ea1
fawzanoachira <fawzanoachira@gmail.com>
2024-06-17 16:36:13 +0530

    asset image

--- listview_whatsapp_ui/lib/listview_object.dart
+++ listview_whatsapp_ui/lib/listview_object.dart
@@ -18,8 +18,10 @@ class ListviewObject extends StatelessWidget {
               leading: const CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.red,
-                backgroundImage: NetworkImage(
-                    "https://flyclipart.com/thumb2/assassins-creed-a-logo-transparent-png-514476.png"),
+                backgroundImage:
+                    AssetImage("assets/images/Assassins-Creed-Symbol.png"),
+                // backgroundImage: NetworkImage(
+                //     "https://flyclipart.com/thumb2/assassins-creed-a-logo-transparent-png-514476.png"),
               ),
               trailing: const Text("5:00 PM"),
             );
